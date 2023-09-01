70790b36c35d9566e3f9d62d242832158104b9cf
shivansh13579 <shivansh0975@gmail.com>
2023-09-01 21:53:17 +0530

    added function in project

---
+++ script.js
@@ -0,0 +1,40 @@
+const randomNumber = parseInt(Math.random()*100 +1);
+
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessField');
+const gessSlot = document.querySelector('.guesses');
+const remaning = document.querySelector('lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+const p = document.createElement('p');
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGames = true;
+
+function validateGuess(guess){
+
+}
+
+function checkGuess(guess){
+
+}
+
+function displayGuess(guess){
+
+}
+
+function displayMessage(message){
+
+}
+
+function newGame(){
+
+}
+
+function endGame(){
+
+}
+
